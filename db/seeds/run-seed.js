ca33727662a8f6600c23f43e01f954ccbde38296
Beau Vincent <beauvincent@yahoo.co.uk>
2025-02-17 15:53:08 +0000

    Reseeded database with dev data instead of test data

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,11 +1,11 @@
 //const devData = require("../data/development-data/index.js");   //Uncomment when developing data for development
-const testData = require("../data/test-data/index.js"); // WHEN TESTING, UNCOMMENT ''testData' AND COMMENT DEV DATA + PAS S IN DEV DATA TO RUN-SEED + TESTING "BEFOREEACH"
+const devData = require("../data/development-data/index.js"); // WHEN TESTING, UNCOMMENT ''testData' AND COMMENT DEV DATA + PAS S IN DEV DATA TO RUN-SEED + TESTING "BEFOREEACH"
 const seed = require("./seed.js");
 const db = require("../connection.js");
 
 const runSeed = () => {
-  //return seed(devData).then(() => db.end()); // Seeding dev data
-  return seed(testData).then(() => db.end()); // Seeding test data (USE WHEN TESTING)
+  //return seed(testData).then(() => db.end()); // Seeding test data
+  return seed(devData).then(() => db.end()); // Seeding dev data (USE WHEN TESTING)
 };
 
 runSeed(); // Exporting seeding func
